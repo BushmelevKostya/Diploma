5c399914a90e0615bdb0241fc26c2427b9628004
Kostya <kostikbushmelev@gmail.com>
2026-04-03 23:36:55 +0300

    feat(tofu): opentofu creates vm on remote libvirt server

--- tofu/modules/vm/main.tf
+++ tofu/modules/vm/main.tf
@@ -1,51 +1,137 @@
-resource "libvirt_volume" "disk" {
-  name           = "${var.name}-disk.qcow2"
-  pool           = var.storage_pool
-  base_volume_id = var.base_image_id
-  format         = "qcow2"
-
-  # Размер в байтах
-  size = var.disk_size_gb * 1024 * 1024 * 1024
+terraform {
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "0.9.7"
+    }
+  }
 }
 
+# Cloud-init ISO
 resource "libvirt_cloudinit_disk" "init" {
+  name = "${var.name}-cloudinit.iso"
+
+  user_data = templatefile("${path.module}/templates/cloud-init.yml.tftpl", {
+    hostname       = var.name
+    ssh_public_key = var.ssh_public_key
+  })
+
+  meta_data = yamlencode({
+    "instance-id"    = var.name
+    "local-hostname" = var.name
+  })
+}
+
+# Upload cloud-init ISO to pool
+resource "libvirt_volume" "cloudinit" {
   name = "${var.name}-cloudinit.iso"
   pool = var.storage_pool
 
-  user_data = var.user_data != "" ? var.user_data : templatefile(
-    "${path.module}/templates/cloud-init.yml.tftpl",
-    {
-      hostname       = var.name
-      ssh_public_key = var.ssh_public_key
+  create = {
+    content = {
+      url = libvirt_cloudinit_disk.init.path
     }
-  )
+  }
 }
 
+# COW disk
+resource "libvirt_volume" "disk" {
+  name          = "${var.name}-disk.qcow2"
+  pool          = var.storage_pool
+  capacity      = var.disk_size_gb * 1024 * 1024 * 1024
+  capacity_unit = "bytes"
+
+  backing_store = {
+    path = var.base_image_path
+    format = {
+      type = "qcow2"
+    }
+  }
+
+  target = {
+    format = {
+      type = "qcow2"
+    }
+  }
+}
+
+# VM
 resource "libvirt_domain" "vm" {
-  name   = var.name
-  memory = var.memory
-  vcpu   = var.vcpu
+  name    = var.name
+  type    = "kvm"
+  memory  = var.memory
+  vcpu    = var.vcpu
+  running = true
 
-  cloudinit = libvirt_cloudinit_disk.init.id
-
-  disk {
-    volume_id = libvirt_volume.disk.id
+  os = {
+    type      = "hvm"
+    type_arch = "x86_64"
   }
 
-  network_interface {
-    network_id     = var.network_id
-    wait_for_lease = true
-  }
+  devices = {
+    disks = [
+      {
+        source = {
+          file = {
+            file = libvirt_volume.disk.path
+          }
+        }
+        target = {
+          dev = "vda"
+          bus = "virtio"
+        }
+        driver = {
+          name = "qemu"
+          type = "qcow2"
+        }
+      },
+      {
+        device = "cdrom"
+        source = {
+          file = {
+            file = libvirt_volume.cloudinit.path
+          }
+        }
+        target = {
+          dev = "sda"
+          bus = "sata"
+        }
+        driver = {
+          name = "qemu"
+          type = "raw"
+        }
+      }
+    ]
 
-  console {
-    type        = "pty"
-    target_port = "0"
-    target_type = "serial"
-  }
+    interfaces = [
+      {
+        source = {
+          network = {
+            network = var.network_name
+          }
+        }
+        model = {
+          type = "virtio"
+        }
+      }
+    ]
 
-  graphics {
-    type        = "vnc"
-    listen_type = "address"
-    autoport    = true
+    consoles = [
+      {
+        target = {
+          type = "serial"
+          port = 0
+        }
+      }
+    ]
+
+    graphics = [
+      {
+        vnc = {
+          auto_port = true
+          listen    = "0.0.0.0"
+        }
+      }
+    ]
   }
 }
--- tofu/modules/vm/outputs.tf
+++ tofu/modules/vm/outputs.tf
@@ -1,19 +1,3 @@
-output "name" {
-  description = "Имя ВМ"
-  value       = libvirt_domain.vm.name
-}
-
-output "id" {
-  description = "ID ВМ в libvirt"
-  value       = libvirt_domain.vm.id
-}
-
-output "ip_address" {
-  description = "IP-адрес ВМ"
-  value       = libvirt_domain.vm.network_interface[0].addresses[0]
-}
-
-output "volume_id" {
-  description = "ID диска (для создания снимков)"
-  value       = libvirt_volume.disk.id
+output "vm_name" {
+  value = libvirt_domain.vm.name
 }
