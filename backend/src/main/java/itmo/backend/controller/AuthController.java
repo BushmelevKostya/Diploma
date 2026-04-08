2b9a7854526ae5d1e6d33af8f1edcac27e2b2f27
Kostya <kostikbushmelev@gmail.com>
2026-04-08 15:38:03 +0300

    feat(back): add auth structure

---
+++ backend/src/main/java/itmo/backend/controller/AuthController.java
@@ -0,0 +1,32 @@
+package itmo.backend.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import itmo.backend.model.dto.auth.JwtResponse;
+import itmo.backend.model.dto.auth.LoginRequest;
+import itmo.backend.model.dto.auth.RefreshTokenRequest;
+import itmo.backend.services.AuthService;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(final AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public JwtResponse login(@Valid @RequestBody final LoginRequest request) {
+        return authService.login(request);
+    }
+
+    @PostMapping("/refresh")
+    public JwtResponse refresh(@Valid @RequestBody final RefreshTokenRequest request) {
+        return authService.refresh(request);
+    }
+}
